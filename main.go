5eb95696245192dc052195d25b62286015ec48d0
Ivan Shashkov <mr.xaski.2002@gmail.com>
2024-04-25 12:52:24 +0700

    CRUD operations

--- main.go
+++ main.go
@@ -1,59 +1,140 @@
 package main
 
 import (
-	"net/url"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
 
 type (
-	CreateLinkRequest struct {
-		External string `json:"external"`
-		Internal string `json:"internal"`
+	GetTaskResponse struct {
+		ID       int64  `json:"id"`
+		Desc     string `json:"description"`
+		Deadline int64  `json:"deadline"`
 	}
 
-	GetLinkResponse struct {
-		Internal string `json:"internal"`
+	CreateTaskRequest struct {
+		Desc     string `json:"description"`
+		Deadline int64  `json:"deadline"`
+	}
+
+	CreateTaskResponse struct {
+		ID int64 `json:"id"`
+	}
+
+	UpdateTaskRequest struct {
+		Desc     string `json:"description"`
+		Deadline int64  `json:"deadline"`
+	}
+
+	Task struct {
+		ID       int64
+		Desc     string
+		Deadline int64
 	}
 )
 
-var links = make(map[string]string)
+var (
+	taskIDCounter int64 = 1
+	tasks               = make(map[int64]Task)
+)
 
 func main() {
 	webApp := fiber.New()
 	webApp.Get("/", func(c *fiber.Ctx) error {
 		return c.SendStatus(200)
 	})
-	// BEGIN (write your solution here)
-	webApp.Post("/links", func(c *fiber.Ctx) error {
-		req := CreateLinkRequest{}
 
+	// BEGIN (write your solution here) (write your solution here)
+	webApp.Post("/tasks", func(c *fiber.Ctx) error {
+		req := CreateTaskRequest{}
 		if err := c.BodyParser(&req); err != nil {
-			return c.Status(fiber.StatusBadRequest).SendString("Invalid JSON")
+			return c.Status(400).SendString("Invalid JSON")
 		}
 
-		links[req.External] = req.Internal
+		CreatedTask := Task{
+			ID:       taskIDCounter,
+			Desc:     req.Desc,
+			Deadline: req.Deadline,
+		}
+		taskIDCounter += 1
+		tasks[CreatedTask.ID] = CreatedTask
+
+		res := CreateTaskResponse{ID: CreatedTask.ID}
+
+		return c.Status(200).JSON(res)
+	})
+
+	webApp.Patch("/tasks/:id", func(c *fiber.Ctx) error {
+		req := c.Params("id", "")
+		reqBody := GetTaskResponse{}
+		if err := c.BodyParser(&reqBody); err != nil {
+			return c.SendStatus(404)
+		}
+
+		reqID, err := strconv.Atoi(req)
+
+		if err != nil {
+			return c.Status(400).SendString("Invalid ID")
+		}
+
+		PatchTask, ok := tasks[int64(reqID)]
+
+		if !ok {
+			return c.SendStatus(404)
+		}
+
+		if reqBody.Desc != "" {
+			PatchTask.Desc = reqBody.Desc
+		}
+		if reqBody.Deadline != 0 {
+			PatchTask.Deadline = reqBody.Deadline
+		}
+
+		tasks[int64(reqID)] = PatchTask
+
 
 		return c.SendStatus(200)
 	})
 
-	webApp.Get("/links/:external", func(c *fiber.Ctx) error {
-		req := c.Params("external", "")
-		request, err := url.QueryUnescape(req)
+	webApp.Get("/tasks/:id", func(c *fiber.Ctx) error {
+		req := c.Params("id", "")
+		reqID, err := strconv.Atoi(req)
+
 		if err != nil {
-			return c.Status(fiber.StatusNotFound).SendString("Link not found")
+			return c.Status(400).SendString("Invalid ID")
 		}
 
-		internal, ok := links[request]
+		GetTask, ok := tasks[int64(reqID)]
 
 		if !ok {
-			return c.Status(fiber.StatusNotFound).SendString("Link not found")
+			return c.SendStatus(404)
 		}
 
-		res := GetLinkResponse{Internal: internal}
-		return c.Status(200).JSON(res)
+		return c.Status(200).JSON(GetTaskResponse(GetTask))
 	})
+
+	webApp.Delete("/tasks/:id", func(c *fiber.Ctx) error {
+		req := c.Params("id", "")
+		reqID, err := strconv.Atoi(req)
+
+		if err != nil {
+			return c.Status(400).SendString("Invalid ID")
+		}
+
+		_, ok := tasks[int64(reqID)]
+
+		if !ok {
+			return c.SendStatus(404)
+		}
+
+		delete(tasks, int64(reqID))
+
+
+		return c.SendStatus(200)
+	})
+
 	// END
 
 	logrus.Fatal(webApp.Listen(":8080"))
